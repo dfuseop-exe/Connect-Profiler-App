8ab73c06fda14ae381afefd4564b99c911cda12c
dfuseop-exe <sushant.165.shinde@gmail.com>
2021-11-18 23:56:56 +0530

    middleware

--- app.js
+++ app.js
@@ -3,11 +3,19 @@ const app = express();
 const port = process.env.PORT || 3000 ;
 
 
+//middleware 
+
+const middleware = (req , res , next)=>{
+    console.log("this is middle ware");
+    next();
+}
+
+
 app.get('/' ,(req , res)=>{
-    res.send("Hello World")
+    res.send("Hello World") 
 })
 
-app.get('/about' , (req , res)=>{
+app.get('/about' , middleware ,  (req , res)=>{
     res.send("This is about Page")
 })
 
