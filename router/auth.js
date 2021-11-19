448f2fe4b6013450b7cdd69009111ea9fcb469e6
dfuseop-exe <sushant.165.shinde@gmail.com>
2021-11-19 15:46:48 +0530

    stored user data in DB using promise

--- router/auth.js
+++ router/auth.js
@@ -1,13 +1,37 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-router.get('/' ,(req , res)=>{
-    res.send("Hello World from router") 
-})
+//link database to this file to work with
+require("../Db/Connection");
+const User = require("../Model/userSchema");
 
-router.post('/register' ,(req , res)=>{
-    res.json({message : req.body})
-    console.log(req.body)
-})
+router.get("/", (req, res) => {
+  res.send("Hello World from router");
+});
+
+router.post("/register", (req, res) => {
+  const { name, email, phone, work, password, cpassword } = req.body;
+
+  //checks is any field is not empty in req.body
+  if (!name || !email || !phone || !work || !password || !cpassword) {
+    return res.status(422).json({ error: "Please fill all fields" });
+  }
+
+  //then check this is already register user or not
+  User.findOne({ email: email }).then((userExist) => {
+    if (userExist) {
+      return res.status(422).json({ message: "Email already exists" });
+    }
+
+    // if not then  take req.body data n save
+    const user = new User({ name, email, phone, work, password, cpassword });
+    user
+      .save().then(() => {
+        return res.status(201).json({ message: "User register successfully" });
+      }).catch((err) => {
+        return res.status(500).json({ error: "failed to register" });
+      });
+  }).catch(err => {console.log(err)})
+});
 
 module.exports = router;
